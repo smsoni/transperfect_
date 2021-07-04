5a75d4a78a8b64f71b00b98bf8b0f03fa0651534
Sheetal <sonimsheetal@gmail.com>
2021-07-04 19:31:59 +0530

    Checking in

--- WahooPageObject/Pages/Cart.cs
+++ WahooPageObject/Pages/Cart.cs
@@ -19,6 +19,8 @@ namespace WahooPageObject.Pages
             get
             {
                 return DriverContext.Driver.FindElement(By.XPath(quantity));
+
+
             }
         }
         public IWebElement UpdateCart
